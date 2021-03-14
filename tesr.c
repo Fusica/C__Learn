dc3184d96fa383b1b300f15077a80a0609f3426d
“Max” <1733169585a@gmail.com>
2021-03-14 18:29:13 +0800

    05 commit

--- tesr.c
+++ tesr.c
@@ -2,22 +2,14 @@
 #include <math.h>
 
 int main() {
-    double a,b,c;
-    scanf("%lf %lf %lf", &a,&b,&c);
-    double x1,x2;
-    if(b*b - 4*a*c == 0){
-        printf("x1=x2=%5.5f", (-b+sqrt(b*b-4*a*c))/(2*a));
-    } else if (b*b - 4*a*c > 0){
-        if ((-b+sqrt(b*b-4*a*c))/(2*a) >= (-b-sqrt(b*b-4*a*c))/(2*a)){
-            x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
-            x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
-        } else{
-            x1 = (-b-sqrt(b*b-4*a*c))/(2*a);
-            x2 = (-b+sqrt(b*b-4*a*c))/(2*a);
-        }
-        printf("x1=%5.5f;x2=%5.5f",x2,x1);
+    float f;
+    scanf("%f",&f);
+    int z;
+    if (f > 0){
+        z = floor(f);
     } else{
-        printf("No answer!");
+        z = ceil(f);
     }
+    printf("%d",z);
     return 0;
 }
