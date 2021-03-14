4b29ccfa425d62561c53898d1e7bb164d4e7c177
“Max” <1733169585a@gmail.com>
2021-03-14 15:27:22 +0800

    02 commit

--- tesr.c
+++ tesr.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int a;
-    double b,c;
-    scanf("%d %lf %lf", &a,&b,&c);
-    int d = ceil(c/b);
-    printf("%d",a-d);
+    char a;
+    scanf("%c", &a);
+    printf("%d",a);
     return 0;
 }
