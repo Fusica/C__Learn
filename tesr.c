253249dd37ed4b000188761bbcba91b69f8a1a88
“Max” <1733169585a@gmail.com>
2021-03-14 16:02:17 +0800

    03 commit

--- tesr.c
+++ tesr.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char a;
-    scanf("%c", &a);
-    printf("%d",a);
+    int a,b,c;
+    scanf("%d %d %d", &a,&b,&c);
+    int d;
+    int rest;
+    if (b%c == 0){
+        d = c/b;
+    } else{
+        d = c/b+1;
+    }
+    if (d < a){
+        rest = a - d;
+    } else{
+        rest = 0;
+    }
+    printf("%d", rest);
     return 0;
 }
